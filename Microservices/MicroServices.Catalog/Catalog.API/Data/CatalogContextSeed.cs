bd113c3e83e122ce10b1661fb3da4cf3743fdd2e
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2021-11-13 13:20:28 -0300

    .NET - Criando Microsserviços : API Catalogo com MongoDB

---
+++ Microservices/MicroServices.Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,60 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (existProduct)
+            {
+                productCollection.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            var products = new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "42242777742101",
+                    Name = "Iphone x",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+                    Image = "product-1.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "422427775577572101",
+                    Name = "Samsung 10",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+                    Image = "product-2.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "422427771172101",
+                    Name = "Samsung 11",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+                    Image = "product-3.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "224274210110",
+                    Name = "Samsung Pocket",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+                    Image = "product-4.png",
+                    Price = 950.00M,
+                    Category = "Smart Phone"
+                }
+            };
+        }
+    }
+}
